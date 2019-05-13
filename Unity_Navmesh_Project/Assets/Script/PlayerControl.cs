00bd53fe7d03d40b4725cef4eb05e38855218612
Raymond <rayfvfree@outlook.com>
2019-05-13 15:10:43 +0800

    Submit

---
+++ Unity_Navmesh_Project/Assets/Script/PlayerControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // Copy from https://answers.unity.com/questions/324589/how-can-i-tell-when-a-navmesh-has-reached-its-dest.html
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    Stop();
+                }
+            }
+        }
+
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(0, 0, agent.speed * Time.deltaTime);
+        	RunAnimation(true);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(0, 0, -agent.speed * Time.deltaTime);
+        	RunAnimation(true);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(0, 90 * Time.deltaTime, 0);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(0, -90 * Time.deltaTime, 0);
+		}
+    }
+
+    public void GoTo(Transform target)
+    {
+        agent.SetDestination(target.position);
+        RunAnimation(true);
+    }
+
+    public void Stop()
+    {
+        RunAnimation(false);
+        agent.ResetPath();
+    }
+
+	void RunAnimation(bool flag){
+		animator.SetBool("Run", flag);
+	}
+
+}
---
+++ Unity_Navmesh_Project/Assets/Script/UIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIScript : MonoBehaviour
+{
+    public RectTransform buttonsPanel;
+    public Button triggerButton;
+
+    bool isOpened = false;
+
+    public void TriggerButtonsPanel()
+    {
+        if(isOpened){
+            buttonsPanel.DOAnchorPosX(230, 1);
+            triggerButton.GetComponentInChildren<Text>().text = "<";
+        }else{
+            buttonsPanel.DOAnchorPosX(0, 1);
+            triggerButton.GetComponentInChildren<Text>().text = ">";
+        }
+        isOpened = !isOpened;
+    }
+
+}
